80b96b686b5de9cfecdcbbf69f8c0735ea1e6be7
Amir.Sh <mobilefactory012@gmail.com>
2025-01-11 13:07:26 +0330

    Add : 4 layer components in index.css

--- src/features/authentication/SendOtpForm.jsx
+++ src/features/authentication/SendOtpForm.jsx
@@ -13,14 +13,12 @@ const SendOtpForm = () => {
             id="phonenumber"
             value={phonenumber}
             onChange={(e) => setPhonenumber(e.target.value)}
-            className="w-full py-3 px-4 rounded-xl text-secondary-900 border hover:border-primary-500 focus:outline-none focus:border-primary-500 focus:bg-white focus:shadow-lg focus:shadow-primary-200 transition-all duration-300 ease-in-out"
+            className="textField__input"
             type="text"
             name="phone"
           />
         </div>
-        <button
-          className="w-full px-4 py-2 font-bold bg-primary-900 text-white rounded-xl transition-all duration-300 ease-in-out hover:bg-primary-800 focus:outline-none focus:bg-primary-700 shadow-lg shadow-primary-300"
-          type="submit">
+        <button className="btn btn--primary w-full" type="submit">
           ارسال کد تایید
         </button>
       </form>
