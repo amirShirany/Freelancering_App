e0830b7c7e58010b3738deb033d9218b2c00c024
Amir.Sh <mobilefactory012@gmail.com>
2025-01-20 14:37:14 +0330

    Add : Home & NotFound page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "react-hot-toast"
+import Home from "./pages/Home"
+import NotFound from "./pages/NotFound"
 import Auth from "./pages/auth"
 import CompleteProfile from "./pages/CompleteProfile"
 
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/auth" element={<Auth />} />
           <Route path="/complete-profile" element={<CompleteProfile />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
     </QueryClientProvider>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter } from "react-router-dom"
 import { createRoot } from "react-dom/client"
-import { StrictMode } from "react"
+// import { StrictMode } from "react"
 import "./index.css"
 import App from "./App.jsx"
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    {/* <StrictMode> */}
+    <App />
+    {/* </StrictMode> */}
   </BrowserRouter>
 )
