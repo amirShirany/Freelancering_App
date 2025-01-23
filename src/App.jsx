80da927f5b70a363e25001615385690e6c6a6dd4
Amir.Sh <mobilefactory012@gmail.com>
2025-01-23 16:05:40 +0330

    Add : owner route structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { Toaster } from "react-hot-toast"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
 import AppLayout from "./ui/AppLayout"
-import Owner from "./pages/Owner"
 import Auth from "./pages/auth"
 import CompleteProfile from "./pages/CompleteProfile"
+import Projects from "./pages/Projects"
+import Project from "./pages/Project"
+import OwnerDashboard from "./pages/OwnerDashboard"
 
 //? Create a new instance of QueryClient
 const queryClient = new QueryClient()
@@ -16,8 +18,11 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Toaster />
       <Routes>
-        <Route element={<AppLayout />}>
-          <Route path="/owner" element={<Owner />} />
+        <Route path="/owner" element={<AppLayout />}>
+          <Route index element={<Navigate to="dashboard" replace />} />
+          <Route path="dashboard" element={<OwnerDashboard />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="projects/:id" element={<Project />} />
         </Route>
 
         <Route path="/auth" element={<Auth />} />
--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -1,5 +1,41 @@
+import { HiCollection, HiHome } from "react-icons/hi"
+import { NavLink } from "react-router-dom"
+
 function Sidebar() {
-  return <div className="bg-secondary-200 row-start-1 row-span-2">Sidebar</div>
+  const navlinkClass =
+    "flex items-center gap-x-2 transition-all duration-300 hover:bg-primary-200/50 hover:text-primary-900 px-2 py-1.5 rounded-lg"
+
+  return (
+    <div className="bg-secondary-200 row-start-1 row-span-2">
+      <ul className="flex flex-col items-start p-8 gap-y-2">
+        <li className="w-full">
+          <NavLink
+            to="/owner/dashboard"
+            className={({ isActive }) =>
+              isActive
+                ? `${navlinkClass} bg-primary-200 text-primary-900`
+                : navlinkClass
+            }>
+            <HiHome />
+            <span>داشبورد</span>
+          </NavLink>
+        </li>
+
+        <li className="w-full">
+          <NavLink
+            to="/owner/projects"
+            className={({ isActive }) =>
+              isActive
+                ? `${navlinkClass} bg-primary-200 text-primary-900`
+                : navlinkClass
+            }>
+            <HiCollection />
+            <span>پروژه‌ ها</span>
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  )
 }
 
 export default Sidebar
