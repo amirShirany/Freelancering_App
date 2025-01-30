5b3e576e4171a8d342aeeeb9a337f20f2ff3918a
Amir.Sh <mobilefactory012@gmail.com>
2025-01-30 16:17:15 +0330

    Add: react-hook-form and CreateProjectForm component

---
+++ src/features/projects/CreateProjectForm.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react"
+import TextField from "../../ui/TextField"
+
+function CreateProjectForm() {
+  const [title, setTitle] = useState()
+
+  return (
+    <form>
+      <TextField
+        label="عنوان پروژه"
+        name="title"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+    </form>
+  )
+}
+
+export default CreateProjectForm
