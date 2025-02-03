5cd0c7e8d3cc9d8580e1cae6258912e3bffaf475
Amir.Sh <mobilefactory012@gmail.com>
2025-02-03 16:40:03 +0330

    Add :use react form hook in TextField

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -1,19 +1,41 @@
-import { useState } from "react"
 import TextField from "../../ui/TextField"
+import { useForm } from "react-hook-form"
+import PropTypes from "prop-types"
+import { data } from "autoprefixer"
 
 function CreateProjectForm() {
-  const [title, setTitle] = useState()
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
 
   return (
-    <form>
+    <form className="space-y-8" onSubmit={handleSubmit(onSubmit)}>
       <TextField
         label="عنوان پروژه"
+        onSubmit={handleSubmit(data)}
         name="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
+        register={register}
+        validationSchema={{
+          required: "عنوان پروژه الزامی است",
+          minLength: { value: 10, message: "حداقل 10 کاراکتر" },
+        }}
+        required
+        errors
       />
+      <button type="submit" className="btn btn--primary w-full ">
+        تایید
+      </button>
     </form>
   )
 }
 
 export default CreateProjectForm
+CreateProjectForm.propTypes = {
+  register: PropTypes.func.isRequired,
+}
--- src/ui/TextField.jsx
+++ src/ui/TextField.jsx
@@ -1,21 +1,46 @@
-function TextField({ name, label, value, onchange }) {
+import PropTypes from "prop-types"
+
+function TextField({
+  name,
+  label,
+  register,
+  validationSchema,
+  type = "text",
+  required,
+  errors,
+}) {
   return (
     <>
       <div>
-        <label className="mb-2 block" htmlFor={name}>
+        <label className="mb-2 block text-secondary-700" htmlFor={name}>
           {label}
+          {required && <span className="text-error">*</span>}
         </label>
         <input
-          type="text"
+          {...register(name, validationSchema)}
           className="textField__input"
           id={name}
-          value={value}
-          onChange={onchange}
           name={name}
+          type={type}
           autoComplete="off"
         />
+        {errors && errors[name] && (
+          <span className="text-error block text-sm mt-2">
+            {errors[name]?.message}
+          </span>
+        )}
       </div>
     </>
   )
 }
 export default TextField
+
+TextField.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  register: PropTypes.func.isRequired,
+  type: PropTypes.string,
+  required: PropTypes.bool,
+  validationSchema: PropTypes.object,
+  errors: PropTypes.object,
+}
