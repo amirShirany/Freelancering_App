5dd7f899215f648a7ded593f2d6be0725cd39546
Amir.Sh <mobilefactory012@gmail.com>
2025-02-10 17:32:06 +0330

    Add : RHFSelect with CreateProjectForm

--- src/features/projects/CreateProjectForm.jsx
+++ src/features/projects/CreateProjectForm.jsx
@@ -1,7 +1,11 @@
-import TextField from "../../ui/TextField"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
-import PropTypes from "prop-types"
 import { data } from "autoprefixer"
+import { WithContext as ReactTags } from "react-tag-input"
+import TextField from "../../ui/TextField"
+import RHFSelect from "../../ui/RHFSelect"
+import PropTypes from "prop-types"
+import DatePickerField from "../../ui/DatePickerField"
 
 function CreateProjectForm() {
   const {
@@ -10,8 +14,30 @@ function CreateProjectForm() {
     handleSubmit,
   } = useForm()
 
+  const [tags, setTags] = useState([
+    { id: "1", text: "JavaScript" },
+    { id: "2", text: "React" },
+  ])
+  const [date, setDate] = useState(new Date())
+
   const onSubmit = (data) => {
     console.log(data)
+  }
+
+  //tag Function
+  const handleDelete = (i) => {
+    setTags(tags.filter((tag, index) => index !== i))
+  }
+
+  const handleAddition = (tag) => {
+    setTags([...tags, tag])
+  }
+
+  const handleDrag = (tag, currPos, newPos) => {
+    const newTags = tags.slice()
+    newTags.splice(currPos, 1)
+    newTags.splice(newPos, 0, tag)
+    setTags(newTags)
   }
 
   return (
@@ -28,6 +54,53 @@ function CreateProjectForm() {
         required
         errors
       />
+      <TextField
+        label="توضیحات"
+        onSubmit={handleSubmit(data)}
+        name="description"
+        register={register}
+        validationSchema={{
+          required: "توضیحات پروژه الزامی است",
+          minLength: { value: 20, message: "حداقل 20 کاراکتر" },
+        }}
+        required
+        errors
+      />
+      <TextField
+        label="بودجه"
+        onSubmit={handleSubmit(data)}
+        name="price"
+        register={register}
+        validationSchema={{
+          required: "توضیحات پروژه الزامی است",
+          minLength: { value: 20, message: "حداقل 20 کاراکتر" },
+        }}
+        required
+        errors
+      />
+      <RHFSelect
+        label="دسته بندی"
+        name="category"
+        register={register}
+        options={[]}
+        required
+        errors
+      />
+      <div>
+        <label className="mb-2 block text-secondary-700">تگ</label>
+        <ReactTags
+          classNames={{ tagInput: "textField__input" }}
+          tags={tags}
+          handleDelete={handleDelete}
+          handleAddition={handleAddition}
+          handleDrag={handleDrag}
+          inputFieldPosition="top"
+          editable
+          maxTags={7}
+        />
+      </div>
+
+      <DatePickerField label="ددلاین" date={date} setDate={setDate} />
       <button type="submit" className="btn btn--primary w-full ">
         تایید
       </button>
---
+++ src/ui/RHFSelect.jsx
@@ -0,0 +1,35 @@
+import PropTypes from "prop-types"
+
+function RHFSelect({ label, name, register, options, required }) {
+  return (
+    <div>
+      <label htmlFor={name} className="mb-2 block text-secondary-700">
+        {label}
+        {required && <span className="text-error">*</span>}
+      </label>
+
+      <select {...register(name)} id={name} className="textField__input">
+        {options.map((option) => (
+          <option key={option.value} value={option.value}>
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
+  )
+}
+
+export default RHFSelect
+
+RHFSelect.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.string.isRequired,
+      label: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  required: PropTypes.bool,
+  register: PropTypes.func.isRequired,
+}
